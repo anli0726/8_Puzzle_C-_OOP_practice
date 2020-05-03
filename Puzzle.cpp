2ccd4e3eaf47d495072860136387a3fa315504d4
An Li <anli4890@hotmail.com>
2020-05-03 00:31:10 -0400

    Update Puzzle.cpp

--- Puzzle.cpp
+++ Puzzle.cpp
@@ -3,26 +3,30 @@
 #include "Puzzle.h"
 using namespace std;
 
-Puzzle::Puzzle(PuzzleState inputState){
+Puzzle::Puzzle(PuzzleState inputState) {
 	setState(inputState);
 }
 
-void Puzzle::setState(PuzzleState inputState){
+
+void Puzzle::setState(PuzzleState inputState)
+{
 	state = inputState;
 	string ID; 
-	for (auto & row : inputState){
-		for (auto & column : row){
+	for (auto & row : inputState)
+		for (auto & column : row)
 			ID += to_string(column);
-		}
-	}
 	stateID = ID;
 }
 
-Puzzle::PuzzleState Puzzle::getState() const{ return state;}
 
-string Puzzle::getID() const{ return stateID;}
+Puzzle::PuzzleState Puzzle::getState() const {return state;}
 
-void Puzzle::printState() const{	
+
+string Puzzle::getID() const {return stateID;}
+
+
+void Puzzle::printState() const
+{	
 	for (auto const & row : getState())
 	{
 		for (auto const & column : row)
@@ -31,12 +35,17 @@ void Puzzle::printState() const{
 	}
 }
 
-array<size_t, 2> Puzzle::getEmptyIdx() const{
+
+array<size_t, 2> Puzzle::getEmptyIdx() const
+{
 	array<size_t, 2> empty_idx;
 	unsigned int i = 0;
-	for (auto & row : state){
-		for (auto & column : row){	
-			if (column == 0){
+	for (auto & row : state)
+	{
+		for (auto & column : row)
+		{	
+			if (column == 0)
+			{
 				empty_idx[0] = i / 3;
 				empty_idx[1] = i % 3;
 				break;
@@ -44,22 +53,28 @@ array<size_t, 2> Puzzle::getEmptyIdx() const{
 			++i;
 		}
 	}
+	
 	return empty_idx;
 }
 
-void Puzzle::printEmptyIdx() const{
+
+void Puzzle::printEmptyIdx() const
+{
 	auto empty_idx = getEmptyIdx();
 	cout << "The empty place is at (" 
 		<< empty_idx[0] << "," 
 		<< empty_idx[1] << ")" << endl;
 }
 
-Puzzle::PuzzleState	Puzzle::move(const char & action) const{
+Puzzle::PuzzleState	Puzzle::move(const char & action) const
+{
 	Puzzle future(state);
 	auto empty_idx = getEmptyIdx();
-	switch (action){
+	switch (action)
+	{
 		case 'u':
-			if (empty_idx[0] != 0){
+			if (empty_idx[0] != 0)
+			{
 				auto temp = future.state[empty_idx[0] - 1][empty_idx[1]];
 				future.state[empty_idx[0] - 1][empty_idx[1]] 
 					= future.state[empty_idx[0]][empty_idx[1]];
@@ -70,7 +85,8 @@ Puzzle::PuzzleState	Puzzle::move(const char & action) const{
 			break;
 				
 		case 'd':
-			if (empty_idx[0] != 2){
+			if (empty_idx[0] != 2)
+			{
 				auto temp = future.state[empty_idx[0] + 1][empty_idx[1]];
 				future.state[empty_idx[0] + 1][empty_idx[1]] 
 					= future.state[empty_idx[0]][empty_idx[1]];
@@ -81,7 +97,8 @@ Puzzle::PuzzleState	Puzzle::move(const char & action) const{
 			break;
 
 		case 'l':
-			if (empty_idx[1] != 0){
+			if (empty_idx[1] != 0)
+			{
 				auto temp = future.state[empty_idx[0]][empty_idx[1] - 1];
 				future.state[empty_idx[0]][empty_idx[1] - 1] 
 					= future.state[empty_idx[0]][empty_idx[1]];
@@ -92,7 +109,8 @@ Puzzle::PuzzleState	Puzzle::move(const char & action) const{
 			break;
 
 		case 'r':
-			if (empty_idx[1] != 2){
+			if (empty_idx[1] != 2)
+			{
 				auto temp = future.state[empty_idx[0]][empty_idx[1] + 1];
 				future.state[empty_idx[0]][empty_idx[1] + 1] 
 					= future.state[empty_idx[0]][empty_idx[1]];
@@ -102,9 +120,10 @@ Puzzle::PuzzleState	Puzzle::move(const char & action) const{
 			}
 			break;
 
-		default: break;
+		default: 
+			break;
 	}
-
+	
 	return future.state;
 }
 
